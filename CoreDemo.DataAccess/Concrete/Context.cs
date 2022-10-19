5bfd41164dff0f60f943ee05faef70ba8e38c8d8
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-10-19 16:52:24 +0300

    CoreDemo.DataAccess - Four Nuget Packages added, folders created and context class added.

---
+++ CoreDemo.DataAccess/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreDemo.DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; database=CoreBlogDb; integrated security=true");
+        }
+    }
+}
