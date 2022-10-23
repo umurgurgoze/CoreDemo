41258f63cfc72efd95e659ef270c6c1a78b081b9
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-10-23 19:22:44 +0300

    CoreDemo.Business - IBlogService and Manager created. CoreDemo- Get method implemented to BlogController for review on index page.

---
+++ CoreDemo.Business/Abstract/IBlogService.cs
@@ -0,0 +1,18 @@
+﻿using CoreDemo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Business.Abstract
+{
+    public interface IBlogService
+    {
+        void BlogAdd(Blog blog);
+        void BlogDelete(Blog blog);
+        void BlogUpdate(Blog blog);
+        List<Blog> GetList();
+        Blog GetById(int id);
+    }
+}
---
+++ CoreDemo.Business/Concrete/BlogManager.cs
@@ -0,0 +1,50 @@
+﻿using CoreDemo.Business.Abstract;
+using CoreDemo.DataAccess.Abstract;
+using CoreDemo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Business.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public BlogManager()
+        {
+
+        }
+        public void BlogAdd(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogDelete(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogUpdate(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogDal.GetListAll();
+        }
+    }
+}
