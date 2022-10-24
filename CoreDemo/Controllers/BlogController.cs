3a15f817fdfdf9268c3b9f77e22e7ea15af9b19e
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-10-24 16:42:16 +0300

    CoreDemo - Some View Components added.

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -14,6 +14,7 @@ namespace CoreDemo.Controllers
         }
         public IActionResult BlogReadAll(int id)
         {
+            ViewBag.Id = id;
             var values = bm.GetBlogByID(id);
             return View(values);
         }
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>()
+            {
+                new UserComment()
+                {
+                   Id = 1,
+                   Username = "Murat"
+                },
+                new UserComment()
+                {
+                   Id = 2,
+                   Username = "Mesut"
+                },
+                new UserComment()
+                {
+                   Id = 3,
+                   Username = "Merve"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}
