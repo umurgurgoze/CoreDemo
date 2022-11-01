dad469fbead48855ebb8f37b779c63f2080dc26b
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-11-01 10:44:41 +0300

    CoreDemo - Dashboard Controller and view components added.

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using CoreDemo.Business.Concrete;
+using CoreDemo.DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ CoreDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using CoreDemo.Business.Concrete;
+using CoreDemo.DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
