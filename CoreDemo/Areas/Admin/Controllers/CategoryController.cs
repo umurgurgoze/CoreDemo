869782568b3a0eb6e3017df40e36bf829e0c0771
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-11-10 12:58:19 +0300

    CoreDemo - In Areas Excel List completed.

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreDemo.Business.Concrete;
+using CoreDemo.Business.ValidationRules;
+using CoreDemo.DataAccess.EntityFramework;
+using CoreDemo.Entity.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index(int page = 1)
+        {
+            var values = cm.GetList().ToPagedList(page, 3); //(Başlangıç değeri, her sayfada kaç item olacağı)
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult result = cv.Validate(category);
+            if (result.IsValid)
+            {
+                category.CategoryStatus = true;
+                cm.TAdd(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult CategoryDelete(int id)
+        {
+            var blogvalue = cm.TGetById(id);
+            cm.TDelete(blogvalue);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -95,8 +95,12 @@ namespace CoreDemo
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
